9bbbb814b766461f77a0dde0d91b1e1ef8b232ea
BoD <BoD@JRAF.org>
2017-11-18 13:43:25 +0100

    Better gpx formatter

--- routetools-lib/src/main/kotlin/org/jraf/routetools/lib/tools/formatter/GpxFormatter.kt
+++ routetools-lib/src/main/kotlin/org/jraf/routetools/lib/tools/formatter/GpxFormatter.kt
@@ -1,17 +1,38 @@
 package org.jraf.routetools.lib.tools.formatter
 
 import com.mapbox.services.commons.models.Position
+import io.jenetics.jpx.GPX
 import org.jraf.routetools.lib.model.Speed
+import java.io.ByteArrayOutputStream
+import java.nio.charset.StandardCharsets
+
 
 object GpxFormatter : Formatter {
     override fun format(positionList: List<Position>, speed: Speed, delayBetweenPositionsSecond: Int): String {
-        val res = StringBuilder("""<?xml version="1.0" encoding="UTF-8"?>
-<gpx xsi:schemaLocation="http://www.topografix.com/GPX/1/0 http://www.topografix.com/GPX/1/0/gpx.xsd" xmlns="http://www.topografix.com/GPX/1/0" creator="RouteUtil" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" version="1.0">
-""")
-        for ((index, position) in positionList.withIndex()) {
-            res.append("<!-- $index --><wpt lat=\"${position.latitude}\" lon=\"${position.longitude}\"/>\n")
-        }
-        res.append("</gpx>")
-        return res.toString()
+        var time = System.currentTimeMillis()
+        val gpx = GPX.builder()
+                .creator("routetools")
+                .addTrack { track ->
+                    track.addSegment { segment ->
+                        for ((index, position) in positionList.withIndex()) {
+                            segment.addPoint { p ->
+                                p.lat(position.latitude)
+                                        .lon(position.longitude)
+                                        .desc("$index")
+                                        .cmt("$index")
+                                        .name("$index")
+                                        .speed(speed.toMetersPerSecond())
+                                        .time(time)
+                            }
+
+                            time += delayBetweenPositionsSecond * 1000
+                        }
+                    }
+                }
+                .build()
+
+        val output = ByteArrayOutputStream()
+        GPX.write(gpx, output, "\t")
+        return output.toString(StandardCharsets.UTF_8.name())
     }
 }
