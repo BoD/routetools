89de01e7a25d69f2271e791bab1dd63c026f8609
BoD <BoD@JRAF.org>
2017-11-19 20:12:13 +0100

    Do the dates and checksum in TCL

--- routetools-lib/src/main/kotlin/org/jraf/routetools/lib/tools/formatter/AndroidEmulatorFormatter.kt
+++ routetools-lib/src/main/kotlin/org/jraf/routetools/lib/tools/formatter/AndroidEmulatorFormatter.kt
@@ -3,14 +3,14 @@ package org.jraf.routetools.lib.tools.formatter
 import com.mapbox.services.commons.models.Position
 import org.jraf.routetools.lib.model.Speed
 import org.jraf.routetools.lib.tools.BearingInfer
-import java.text.SimpleDateFormat
-import java.util.Date
 import java.util.Locale
 
 
 object AndroidEmulatorFormatter : Formatter {
     override fun format(positionList: List<Position>, speed: Speed, delayBetweenPositionsSecond: Int): String {
         val res = StringBuilder("""#!/usr/bin/expect
+
+namespace path {::tcl::mathop}
 
 set ip [lindex ${'$'}argv 0]
 set port [lindex ${'$'}argv 1]
@@ -20,9 +20,20 @@ set timeout 1
 spawn telnet ${'$'}ip ${'$'}port
 expect_after eof { exit 0 }
 
-## interact
+proc checksum {s} {
+  set cs 0
+  foreach char [split ${'$'}s {}] {
+    scan ${'$'}char "%c" c
+    set cs [^ ${'$'}cs ${'$'}c]
+  }
+  return [format "%02X" ${'$'}cs]
+}
+
+
+# Interact
 expect OK
 
+# Auth token
 set fp [open "~/.emulator_console_auth_token" r]
 if {[gets ${'$'}fp line] != -1} {
   send "auth ${'$'}line\r"
@@ -30,30 +41,21 @@ if {[gets ${'$'}fp line] != -1} {
 
 """)
         val bearingInfer = BearingInfer()
-        val dateStr = SimpleDateFormat("ddMMyy").format(Date())
-        val timeFormat = SimpleDateFormat("hhmmss")
-        var time = System.currentTimeMillis()
         for ((index, position) in positionList.withIndex()) {
             bearingInfer.add(position)
 
-            val timeStr = timeFormat.format(Date(time))
             val latitude = String.format(Locale.US, "%02d%.3f,%s", position.latitudeDegrees, position.latitudeMinutes, position.latitudeNorthSouth)
             val longitude = String.format(Locale.US, "%03d%.3f,%s", position.longitudeDegrees, position.longitudeMinutes, position.longitudeEastWest)
             val bearing = String.format(Locale.US, "%.1f", bearingInfer.bearing)
             val speedStr = String.format(Locale.US, "%.1f", speed.toMetersPerSecond())
 
             res.append("# $index  lat: ${position.latitude} ($latitude)  lon: ${position.longitude} ($longitude)  bear: $bearing  speed: $speedStr\n")
-            val nmeaCommand = "\$GPRMC,$timeStr,A,$latitude,$longitude,$speedStr,$bearing,$dateStr,,*"
-            val nmeaCommandWithChecksum = "$nmeaCommand${getChecksum(nmeaCommand)}"
-            val emulatorCommand = "geo nmea $nmeaCommandWithChecksum"
-            res.append("# $emulatorCommand\n")
-            val escapedEmulatorCommand = emulatorCommand.replace("$", "\\$")
-            res.append("send \"$escapedEmulatorCommand\\r\"\n")
+            res.append("set s \"GPRMC,[clock format [clock seconds] -format %H%M%S],A,$latitude,$longitude,$speedStr,$bearing,\$date,,\"\n")
+            res.append("set s \"geo nmea \\\$\$s*[checksum \$s]\"\n")
+            res.append("send \"\$s\\r\"\n")
             res.append("expect OK\n")
             res.append("sleep $delayBetweenPositionsSecond\n")
             res.append("\n")
-
-            time += delayBetweenPositionsSecond * 1000
         }
 
         res.append("\nsend \"exit\\r\"\n")
@@ -67,15 +69,4 @@ if {[gets ${'$'}fp line] != -1} {
     private val Position.longitudeDegrees get() = Math.abs(longitude).toInt()
     private val Position.longitudeMinutes get() = 60 * (Math.abs(longitude) - longitudeDegrees)
     private val Position.longitudeEastWest get() = if (longitude > 0) "E" else "W"
-
-    private fun getChecksum(command: String): String {
-        val from = command.indexOf("$")
-        val to = command.indexOf("*")
-        val checkString = command.substring(from + 1, to)
-        var sum = 0
-        for (i in 0 until checkString.length) {
-            sum = sum xor checkString[i].toInt()
-        }
-        return String.format("%02X", sum)
-    }
 }
